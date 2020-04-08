12717c5ab9723faf3ba4b56c8cbe316dde446160
Andres <andres.perez.zabala@gmail.com>
2020-04-08 17:27:12 +0200

    Animando a Buster

--- public/js/Player.js
+++ public/js/Player.js
@@ -1,19 +1,37 @@
-
 import {Object2D, Vec2D} from "./math.js";
 import Settings from "./Settings.js";
 
+const  frame = ['buster','buster-1','buster-2','buster-3']
+
 export default class Player extends Object2D {
 
-    constructor(size, pos, sprite) {
+    routeFrame(){
+        if (this.direction.x != 0){
+            const frameIndex = Math.floor(this.distance/10) % frame.length;
+            const frameName = frame[frameIndex];
+            return frameName;
+        }
+
+        return 'buster';
+    }
+
+    constructor(size, pos, spriteSheet) {
         super(size, pos);
         this.force = new Vec2D(0, 0);
-        this.sprite = sprite;
+        this.spriteSheet = spriteSheet;
         this.direction = new Vec2D(0,0);
+        this.distance = 0;
+
     }
 
     // time respresenta el tiempo que ha pasado desde la última ejecución
     update(time) {
 
+        if (this.direction.x != 0){
+            this.distance += Settings.PLAYER_SPEED * time;
+        }else{
+            this.distance = 0;
+        }
         /*
         Asume por el momento que Settings.SCREEN_HEIGHT y Settings.SCREEN_WIDTH indican el tamaño de
         la pantalla del juego. Settings tiene otras constantes definidas (échales un vistazo)
@@ -52,6 +70,6 @@ export default class Player extends Object2D {
 
     draw(context) {
         // pintar this.sprite en el contexto (en posicion x,y)
-        context.drawImage(this.sprite,this.x,this.y);
+        context.drawImage(this.spriteSheet.get(this.routeFrame()),this.x,this.y);
     }
 }
--- public/js/loaders.js
+++ public/js/loaders.js
@@ -17,10 +17,13 @@ export function loadImage(url){
 export function  loadBuster(image) {
     const spriteSheet = new SpriteSheet(image,32,32);
     spriteSheet.define('buster',0,0);
+    spriteSheet.define('buster-1',1,0);
+    spriteSheet.define('buster-2',2,0);
+    spriteSheet.define('buster-3',3,0);
 
     const size = new Vec2D(32,32);
-    const pos = new Vec2D(Settings.SCREEN_WIDTH/2 - size.x, Settings.SCREEN_HEIGHT - size.y);
+    const pos = new Vec2D((Settings.SCREEN_WIDTH - size.x)/2, Settings.SCREEN_HEIGHT - size.y);
 
-    return new Player(size,pos,spriteSheet.get('buster'));
+    return new Player(size,pos,spriteSheet);
 
 }
